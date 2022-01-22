fd18dd4efa94df42a88ba3b3d80d8a4cc09c2096
terje-knutsen <98122448+terje-knutsen@users.noreply.github.com>
2022-01-22 14:36:56 +0100

    Create Value.cs

---
+++ UseLess/UseLess.Framework/Value.cs
@@ -0,0 +1,24 @@
+﻿namespace UseLess.Framework
+{
+    public abstract class Value<T> : IEquatable<T>
+    {
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if(ReferenceEquals(this, obj)) return true;
+            return obj is T other && Equals((T)other);
+        }
+        public bool Equals(T? other)
+        => CompareProperties(other);
+        protected abstract bool CompareProperties(T? other);
+        public override int GetHashCode()
+        {
+            var hashCode = 1779266240;
+            foreach (var prop in GetType().GetProperties())
+                hashCode = hashCode * -1521134295 + prop.GetHashCode();
+            return hashCode;
+        }
+        public static bool operator ==(Value<T> a, Value<T> b) => Equals(a, b);
+        public static bool operator !=(Value<T> a, Value<T> b) => !Equals(a, b);
+    }
+}
