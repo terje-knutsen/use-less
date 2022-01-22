8d7514fc0beb8eaaf7f3b26260428adad44f9cf9
terje-knutsen <98122448+terje-knutsen@users.noreply.github.com>
2022-01-22 14:30:06 +0100

    Create Entity.cs

---
+++ UseLess/UseLess.Framework/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace UseLess.Framework
+{
+    public abstract class Entity<TId> : IInternalEventHandler
+    {
+        private Action<object> applier;
+        public TId Id { get; protected set; }
+        public void SetApplier(IInternalEventHandler handler) => applier = handler.Handle;
+        protected Entity(Action<object> applier) => this.applier = applier;
+        protected abstract void When(object @event);
+        protected void Apply(object @event) 
+        {
+            When(@event);
+            applier(@event);
+        }
+        public void Handle(object @event)
+        => When(@event);
+    }
+}
