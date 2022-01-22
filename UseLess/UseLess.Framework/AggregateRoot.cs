3c85ea314ec5f250340de1aa3dcab601952d31d2
terje-knutsen <98122448+terje-knutsen@users.noreply.github.com>
2022-01-22 14:22:46 +0100

    AggregateRoot

---
+++ UseLess/UseLess.Framework/AggregateRoot.cs
@@ -0,0 +1,33 @@
+﻿namespace UseLess.Framework
+{
+    public abstract class AggregateRoot<TId> : IInternalEventHandler
+    {
+        private readonly List<object> changes;
+        protected AggregateRoot() => changes = new List<object>();
+        void IInternalEventHandler.Handle(object @event) => Apply(@event);
+        protected void Apply(object @event) 
+        {
+            When(@event);
+            EnsureValidState();
+            changes.Add(@event);
+        }
+        protected abstract void When(object @event);
+        protected abstract void EnsureValidState();
+        protected void ApplyToEntity(IInternalEventHandler entity, object @event)
+            => entity?.Handle(@event);
+        protected void ApplyToEntities(IEnumerable<IInternalEventHandler> entities, object @event)
+            => entities.ToList().ForEach(x => x.Handle(@event));
+        public void Load(IEnumerable<object> history) 
+        {
+            foreach(var e in history) 
+            {
+                When(e);
+                Version++;
+            }
+        }
+        public TId Id { get; protected set; }
+        public int Version { get; private set; } = -1;
+        public IEnumerable<object> GetChanges() => changes.AsEnumerable();
+        public void Clear() => changes.Clear();
+    }
+}
---
+++ UseLess/UseLess.Framework/IInternalEventHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace UseLess.Framework
+{
+    public interface IInternalEventHandler
+    {
+        void Handle(object @event);
+    }
+}
