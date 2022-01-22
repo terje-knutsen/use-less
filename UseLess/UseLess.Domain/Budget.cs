1d58632a3429c1efb128f8923af0213fd5f0cebd
terje-knutsen <98122448+terje-knutsen@users.noreply.github.com>
2022-01-22 14:47:33 +0100

    Budget

---
+++ UseLess/UseLess.Domain/Budget.cs
@@ -0,0 +1,18 @@
+﻿using UseLess.Domain.Values;
+using UseLess.Framework;
+
+namespace UseLess.Domain
+{
+    public sealed class Budget : AggregateRoot<BudgetId>
+    {
+        protected override void EnsureValidState()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void When(object @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UseLess/UseLess.Domain/Values/BudgetId.cs
@@ -0,0 +1,19 @@
+﻿using UseLess.Framework;
+
+namespace UseLess.Domain.Values
+{
+    public sealed class BudgetId : Value<BudgetId>
+    {
+        private readonly Guid value;
+        private BudgetId(Guid value)=> this.value = value;
+        protected override bool CompareProperties(BudgetId? other)
+        => value == other?.value;
+        public static BudgetId From(Guid guid)=> new(guid);
+        public static BudgetId From(string value)=> new(Guid.Parse(value));
+        public static implicit operator Guid(BudgetId self)=> self.value;
+        public static implicit operator BudgetId(string value)
+            => new(Guid.Parse(value));
+        public override string ToString()
+        => value.ToString();
+    }
+}
