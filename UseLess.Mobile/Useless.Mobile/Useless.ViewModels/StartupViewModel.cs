2f72e96b57124ca34eac480c308241d3f120e41a
terje-knutsen <98122448+terje-knutsen@users.noreply.github.com>
2022-05-29 16:46:24 +0200

    experiment layout

--- UseLess.Mobile/Useless.Mobile/Useless.ViewModels/StartupViewModel.cs
+++ UseLess.Mobile/Useless.Mobile/Useless.ViewModels/StartupViewModel.cs
@@ -1,10 +1,22 @@
-﻿using Useless.Framework;
+﻿using System.Collections.ObjectModel;
+using Useless.Framework;
 
 namespace Useless.ViewModels
 {
     public sealed class StartupViewModel : BaseViewModel
     {
         public StartupViewModel(INavigationService navService) : base(navService)
-        { }
+        {
+            SelectedCategory = Categories[0];
+        }
+        public ObservableCollection<string> Categories => new ObservableCollection<string> 
+        {
+            "Mat",
+            "Klær",
+            "Barn",
+            "Bil"
+        };
+        public string SelectedCategory { get; set; }
+        public decimal Limit => 155m;
     }
 }
